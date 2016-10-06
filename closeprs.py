4997bf04ffd1a848189ddc7dd8b819e120007329
Nikhil Marathe <nsm.nikhil@gmail.com>
2016-10-05 20:36:32 -0700

    Initial version. does not close yet since that is dangerous.

---
+++ closeprs.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timedelta
+from github import Github
+
+gh = Github("nikhilm", open("token.cfg").read().strip())
+repo = gh.get_repo("iron-io/go")
+
+howlong = timedelta(days=30)
+deadline = datetime.now() - howlong
+print "Deadline", deadline
+for pr in repo.get_pulls():
+    mtime = pr.updated_at
+    if mtime < deadline:
+        print pr.number, pr.title, "will be deleted", mtime
+        # pr.create_issue_comment('Closing this since it was last modified more than %s days ago. This is an automated message.' % howlong.days)
+        # pr.edit(state='closed')
